de8686452d656b475b65d3e753387ccc55268c97
web5bariflo <web5.bariflo@outlook.com>
2025-05-31 10:05:46 +0530

    use db sql

--- pump_project/settings.py
+++ pump_project/settings.py
@@ -78,26 +78,26 @@ WSGI_APPLICATION = 'pump_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-
 DATABASES = {
-    
-    'default':{
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pumpdb',                           
-        'USER': 'Vertoxlabs',
-        'PASSWORD':'Vtx@2025',
-        'HOST': 'bcpostgressqlserver.postgres.database.azure.com',
-        'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+# DATABASES = {
+    
+#     'default':{
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'pumpdb',                           
+#         'USER': 'Vertoxlabs',
+#         'PASSWORD':'Vtx@2025',
+#         'HOST': 'bcpostgressqlserver.postgres.database.azure.com',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
