c09af1d7ae2905670910a35ee5957ceae16eb788
web10-bariflo <web1.bariflo@outlook.com>
2025-11-18 12:38:43 +0530

    database updated

--- pump_project/settings.py
+++ pump_project/settings.py
@@ -99,6 +99,18 @@ DATABASES = {
 #     }
 # }
 
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "pumpdb",
+        "USER": "vertoxl",
+        "PASSWORD": "vertoxlabs",
+        "HOST": "192.168.1.140",
+        "PORT": "5432",
+    }
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
